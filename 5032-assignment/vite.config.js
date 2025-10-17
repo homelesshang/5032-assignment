596c8c7073e2ee3ef259e86de8503cde949c1e94
hangruan <hrua0006@student.monash.edu>
2025-10-17 20:11:25 +1100

    fix the deploy error

--- 5032-assignment/vite.config.js
+++ 5032-assignment/vite.config.js
@@ -2,18 +2,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+let devToolsPlugin = []
+
+if (process.env.NODE_ENV === 'development') {
+  try {
+    const vueDevTools = await import('vite-plugin-vue-devtools')
+    devToolsPlugin = [vueDevTools.default()]
+  } catch (err) {
+    console.warn('⚠️ vue-devtools not loaded in production build')
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [
     vue(),
-    
-    ...(mode === 'development' ? [vueDevTools()] : []),
+    ...devToolsPlugin, 
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-}))
+})
